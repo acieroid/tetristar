2f22b5925fcd0c21a50f7c2d4bd6bdaa6a47ceaf
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-08-19 10:25:06 +0200

    fixing lua plugins to use the new api

--- server/plugins/admin.lua
+++ server/plugins/admin.lua
@@ -2,9 +2,9 @@ admin = {}
 
 function admin.password(id, command, args)
    password = args
-   if pass == server.get_password() then
+   if pass == tetris.server.get_password() then
       tetris.players.set_admin(id)
    end
 end
 
-server.register("RECV", admin.password, "PASSWORD")
+tetris.plugin.register("RECV", admin.password, "PASSWORD")
--- server/plugins/connection.lua
+++ server/plugins/connection.lua
@@ -7,17 +7,17 @@ function connection.hello(id, command, args)
    if not tetris.players.exists(id) then
      if tetris.players.nick_available(nick) then
         -- Send his nick to the player
-        server.send(id, "HELLO " .. id .. " " .. nick)
+        tetris.server.send(id, "HELLO " .. id .. " " .. nick)
         -- Send all the players connected to the player
         for i,player_id in pairs(tetris.players.all()) do
            player_nick = tetris.players.get_nick(player_id) 
-           server.send(id, "NEWPLAYER " .. player_id .. " " .. player_nick)
+           tetris.server.send(id, "NEWPLAYER " .. player_id .. " " .. player_nick)
         end
         -- Add the player
         tetris.players.add(id)
         tetris.players.set_nick(id, nick)
         -- Tell the other players about this one
-        server.send_to_all("NEWPLAYER " .. id .. " " .. nick)
+        tetris.server.send_to_all("NEWPLAYER " .. id .. " " .. nick)
      else
         -- Nick already taken, try appending a '_'
         connection.hello(id, command, args .. "_")
@@ -30,5 +30,5 @@ function connection.disconnect(id)
    server.send_to_all("BYE " .. id)
 end
 
-server.register("DISCONNECT", connection.disconnect)
-server.register("RECV", connection.hello, "HELLO")
+tetris.plugin.register("DISCONNECT", connection.disconnect)
+tetris.plugin.register("RECV", connection.hello, "HELLO")
