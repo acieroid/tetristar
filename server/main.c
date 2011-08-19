ad1c4715e578ff2cc726b3eb8e86a743dc014e2d
Quentin Stievenart <quentin.stievenart@gmail.com>
2011-08-19 10:04:20 +0200

    adding some debug statements

--- server/main.c
+++ server/main.c
@@ -13,26 +13,46 @@ static lua_State *lua_state;
 void tetristar_init(gchar *configfile)
 {
   g_debug("Initializing tetristar server");
+
+  g_debug("Inilializing lua");
   lua_state = lua_open();
   luaL_openlibs(lua_state);
+
+  g_debug("Initializing configuration");
   config_init(lua_state, configfile);
+
+  g_debug("Initializing libtetris");
   tetris_init();
   tetris_plugin_init(lua_state);
   tetris_id_init(config_get_int("max_clients",
                                 default_max_clients));
+
+  g_debug("Initializing server plugins");
   plugins_init();
+
+  g_debug("Initializing network");
   network_init();
 }
 
 void tetristar_deinit()
 {
   g_debug("Shutting down tetristar server");
+
+  g_debug("Shutting down network");
   network_deinit();
+
+  g_debug("Shutting down server plugins");
   plugins_deinit();
+
+  g_debug("Shutting down libtetris");
   tetris_deinit();
   tetris_plugin_deinit();
   tetris_id_deinit();
+
+  g_debug("Shutting down configuration");
   config_deinit();
+
+  g_debug("Shutting down lua");
   lua_close(lua_state);
 }
 
