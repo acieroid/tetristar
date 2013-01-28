5241d9aabbdb1e64b8851bcc7ec808073ff42a20
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-01-29 00:01:48 +0100

    Fix kept piece

--- server/plugins/kept.lua
+++ server/plugins/kept.lua
@@ -3,16 +3,16 @@ kept = {}
 -- Keep the current piece, swapping it with the previously kept piece
 -- (or with a new one if no piece has been kept previously)
 function kept.keep(id, command, args)
-   local kept = tetris.player.get_kept_piece(id)
+   local piece = tetris.player.get_kept_piece(id)
    tetris.server.send(id,
-                      string.format("KEPT %s", game.to_fieldspec(kept)))
-   tetris.player.set_kept_piece(tetris.player.get_piece(id))
-   if kept == {} then
+                      string.format("KEPT %d %s", id, game.to_fieldspec(piece)))
+   tetris.player.set_kept_piece(id, tetris.player.get_piece(id))
+   if #piece == 0 then
       field.new_piece(id)
       game.send_piece(id)
       game.send_next_piece(id)
    else
-      tetris.player.set_piece(id, kept)
+      tetris.player.set_piece(id, piece)
    end
 end
 
